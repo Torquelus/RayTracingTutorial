cc14a72f01b7ab784fa284580d17dabdc8266021
Torquelus <alari.tork@gmail.com>
2019-05-13 09:15:41 -0400

    Full Implementation of Vector3

--- RayTracingTutorial/Vector3.cpp
+++ RayTracingTutorial/Vector3.cpp
@@ -7,7 +7,75 @@ Vector3::Vector3(float e0, float e1, float e2){
 	e[2] = e2;
 }
 
+// Operator Overloads
 
+inline Vector3& Vector3::operator+=(const Vector3& v2) {
+	e[0] += v2.e[0];
+	e[1] += v2.e[1];
+	e[2] += v2.e[2];
+	return *this;
+}
+
+inline Vector3& Vector3::operator-=(const Vector3& v2) {
+	e[0] -= v2.e[0];
+	e[1] -= v2.e[1];
+	e[2] -= v2.e[2];
+	return *this;
+}
+
+inline Vector3& Vector3::operator*=(const Vector3& v2) {
+	e[0] *= v2.e[0];
+	e[1] *= v2.e[1];
+	e[2] *= v2.e[2];
+	return *this;
+}
+
+inline Vector3& Vector3::operator/=(const Vector3& v2) {
+	e[0] /= v2.e[0];
+	e[1] /= v2.e[1];
+	e[2] /= v2.e[2];
+	return *this;
+}
+
+inline Vector3& Vector3::operator*=(const float t) {
+	e[0] *= t;
+	e[1] *= t;
+	e[2] *= t;
+	return *this;
+}
+
+inline Vector3& Vector3::operator/=(const float t) {
+	float k = 1.0 / t;
+
+	e[0] *= k;
+	e[1] *= k;
+	e[2] *= k;
+
+	return *this;
+}
+
+// Vector3 Methods
+
+// Length of Vector3
+inline float Vector3::length() const{
+	return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+}
+
+// Squared Length of Vector3
+inline float Vector3::squaredLength() const {
+	return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+}
+
+// Make Unit Vector
+inline void Vector3::makeUnitVector() {
+	// Find Multiplier
+	float k = 1.0 / length();
+
+	// Apply Multiplier
+	e[0] *= k;
+	e[1] *= k;
+	e[2] *= k;
+}
 
 // Destructor
 Vector3::~Vector3(){
--- RayTracingTutorial/Vector3.h
+++ RayTracingTutorial/Vector3.h
@@ -19,11 +19,27 @@ public:
 	inline float g() const { return e[1]; }
 	inline float b() const { return e[2]; }
 
+	// Basic Operator Overloads
+	inline const Vector3& operator+() const { return *this; }
+	inline Vector3 operator-() const { return Vector3(-e[0], -e[1], -e[2]); }
+	inline float operator[](int i) { return e[i]; }
 
+	// Other Operator Overloads
+	inline Vector3& operator+=(const Vector3& v2);
+	inline Vector3& operator-=(const Vector3& v2);
+	inline Vector3& operator*=(const Vector3& v2);
+	inline Vector3& operator/=(const Vector3& v2);
+	inline Vector3& operator*=(const float t);
+	inline Vector3& operator/=(const float t);
+
+	// Vector3 Methods
+	inline float length() const;
+	inline float squaredLength() const;
+	inline void makeUnitVector();
 
 	// Destructor
 	~Vector3();
-private:
+
 	// Holds Values
 	float e[3];
 };
